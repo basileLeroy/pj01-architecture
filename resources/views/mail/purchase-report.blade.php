8f383858ea054e5f09b1dcfd8c492bd7f635f6ba
Basile <basile.leroy.jnm@hotmail.com>
2021-11-22 16:24:28 +0100

    Changing link url from mail button

--- resources/views/mail/purchase-report.blade.php
+++ resources/views/mail/purchase-report.blade.php
@@ -17,7 +17,7 @@
 > {{ $street }}, {{ $zip }} {{ $city }} <br>
 > {{ $region }} - {{ $country }} <br>
 
-@component('mail::button', ['url' => config('app.url')])
+@component('mail::button', ['url' => config('app.url') . ':8000'])
 Bevestig dat de bestelling is verzonden naar klant
 @endcomponent
 
