699277d6ff65c680222695587a0db4631794b401
Basile <basile.leroy.jnm@hotmail.com>
2021-11-22 13:26:32 +0100

    Fixing mail subject translations

--- app/Mail/ConfirmationOrder.php
+++ app/Mail/ConfirmationOrder.php
@@ -6,6 +6,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Lang;
 
 class ConfirmationOrder extends Mailable
 {
@@ -55,6 +56,6 @@ class ConfirmationOrder extends Mailable
     public function build()
     {
         return $this->markdown('mail.confirmation-order')
-            ->subject("Architecturer - {{__('order.title')}}");
+            ->subject('Architecturer - ' . Lang::get('order.title') );
     }
 }
