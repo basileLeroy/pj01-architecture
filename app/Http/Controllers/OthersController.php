41a1249329d5baa00d3e6826fb3615f3a40f2d55
basileLeroy <basile.leroy.jnm@hotmail.com>
2022-05-15 12:25:23 +0200

    Udate other controller: adding detail pages

--- app/Http/Controllers/OthersController.php
+++ app/Http/Controllers/OthersController.php
@@ -12,40 +12,49 @@ class OthersController extends Controller
     public function showOthers()
     {
         $page = 'others';
+        $detailPage= "others-detail";
         $localeLanguage = App::getLocale();
 
         $articles = Article::where('language', '=', $localeLanguage)
             ->where('page', '=', $page)
             ->get();
 
+        $detailArticles = Article::where('language', '=', $localeLanguage)
+            ->where('page', '=', $detailPage)
+            ->get();
+
         return view('others')->with([
-            'articles' => $articles]);
+            'articles' => $articles,
+            'detailPages' => $detailArticles
+        ]);
     }
 
     public function updateOthers(Request $request)
     {
         $page = 'others';
         $localeLanguage = App::getLocale();
+        $noContentMessage = "";
         $articles = Article::where('language', '=', $localeLanguage)
             ->where('page', '=', $page)
             ->first();
-//        dd($localeLanguage, $articles, $request);
-        $request->validate([
-            'image' => 'image|mimes:jpg,png,jpeg|max:5048',
-        ]);
 
-        $addNewImage = null;
+        if($request->newArticle) {
 
-        $imageExists = Storage::exists($articles->image);
+            $detailTitle =  str_replace(" ", "-", $request->newArticle);
 
-
-        if($request->image) {
-            if($imageExists) {
-                Storage::delete($articles->image);
+            if($localeLanguage === "nl") {
+                $noContentMessage = "Er is nog geen inhoud beschikbaar.";
+            } else if ($localeLanguage === "fr") {
+                $noContentMessage = "Aucun contenu disponible pour le moment.";
+            } else {
+                $noContentMessage = "No content available yet.";
             }
-            $addNewImage = $request->file('image')->store('images/architecture/articles');
-
-            $articles->image = $addNewImage;
+            Article::create([
+                'title' => $detailTitle,
+                'article_content' => $noContentMessage,
+                'language' => $localeLanguage,
+                'page' => "others-detail"
+            ]);
         };
         $articles->article_content = $request->description;
         $articles->save();
