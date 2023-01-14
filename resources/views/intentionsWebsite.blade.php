21aa3eb3702d24b3f0aa63e519d2526e27a26e5b
basileLeroy <basile.leroy.jnm@hotmail.com>
2023-01-14 20:06:13 +0100

    WIP - Add responsiveness to intention pages

--- resources/views/intentionsWebsite.blade.php
+++ resources/views/intentionsWebsite.blade.php
@@ -10,27 +10,28 @@
 @endsection
 
 @section('content')
-    @auth
-        <div class="editSection w3-display-container">
-            <input class="toggle-box" id="header1" type="checkbox" >
-            <label for="header1"><i class="fa fa-edit w3-xxlarge w3-display-topleft"></i></label>
-            <div class="addSection">
-                <form action="{{ route('intenties-van-de-site', ['locale' => app()->getLocale()] ) }}" method="POST">
-                {{ csrf_field() }}
-                    <textarea class="description" id="sectionContent" name="description">
+{{--    style width = 650px--}}
+    <div class="content">
+        @auth
+            <div class="editSection w3-display-container">
+                <input class="toggle-box" id="header1" type="checkbox" >
+                <label for="header1"><i class="fa fa-edit w3-xxlarge w3-display-topleft"></i></label>
+                <div class="addSection">
+                    <form action="{{ route('intenties-van-de-site', ['locale' => app()->getLocale()] ) }}" method="POST">
+                        {{ csrf_field() }}
+                        <textarea class="description" id="sectionContent" name="description">
                         @foreach ($articles as $article)
-                            <?= $article->article_content ?>
+
+                            {!! $article->article_content !!}
                         @endforeach
                     </textarea>
-                    <button type="submit" id="uploadNewSection" name="uploadNewProject" value="Upload">Save</button>
-                </form>
+                        <button type="submit" id="uploadNewSection" name="uploadNewProject" value="Upload">Save</button>
+                    </form>
+                </div>
             </div>
-        </div>
-    @endauth
-    <div class="fluid intro" style="width: 650px;">
-
+        @endauth
         @foreach ($articles as $article)
-            <?= $article->article_content ?>
+            {!! $article->article_content !!}
         @endforeach
     </div>
 @endsection
