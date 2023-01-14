0cde742554230fb94d5fbe753a12b3280b8850e3
basileLeroy <basile.leroy.jnm@hotmail.com>
2023-01-14 19:36:48 +0100

    WIP - Add responsiveness to landing page

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -14,29 +14,28 @@ use GuzzleHttp\Psr7\Uri;
 @endsection
 
 @section('content')
-    <div class="fluid intro">
-        <img src="{{url('/images/home/allessandro.jpg')}}" title="Les ordres - Alessandro Anselmi" alt="Allessandro Anselmi">
-        <input class="toggle-box" id="header1" type="checkbox" >
-        <label for="header1">{{$article->title}}</label>
-        <div class="article w3-display-container">
-            @auth
-            <div class="editSection w3-display-container">
-                <i class="fa fa-edit w3-xxlarge"></i>
-                <div class="addSection">
-                    <form action="{{ route('home', ['locale' => app()->getLocale()] ) }}" method="POST">
-                    {{ csrf_field() }}
-                        <label for="title">New Title: </label>
-                        <input type="text" id="title" name="title" placeholder="Your new Title here" value="{{$article->title}}" style="border: 1px solid; padding-left:5px;">
-                        <textarea class="description" id="sectionContent" name="description">
-                                {{ $article->article_content }}
-                        </textarea>
-                        <button type="submit" id="uploadNewSection" name="uploadNewProject" value="Upload">Save</button>
-                    </form>
-                </div>
+    <img src="{{url('/images/home/allessandro.jpg')}}" title="Les ordres - Alessandro Anselmi" alt="Allessandro Anselmi">
+{{--    <input class="toggle-box" id="header1" type="checkbox" >--}}
+{{--    <label for="header1">{{$article->title}}</label>--}}
+    <div class="content">
+        <h1>{{$article->title}}</h1>
+        @auth
+        <div class="editSection w3-display-container">
+            <i class="fa fa-edit w3-xxlarge"></i>
+            <div class="addSection">
+                <form action="{{ route('home', ['locale' => app()->getLocale()] ) }}" method="POST">
+                {{ csrf_field() }}
+                    <label for="title">New Title: </label>
+                    <input type="text" id="title" name="title" placeholder="Your new Title here" value="{{$article->title}}" style="border: 1px solid; padding-left:5px;">
+                    <textarea class="description" id="sectionContent" name="description">
+                            {{ $article->article_content }}
+                    </textarea>
+                    <button type="submit" id="uploadNewSection" name="uploadNewProject" value="Upload">Save</button>
+                </form>
             </div>
-            @endauth
-
-            <?= $article->article_content ?>
         </div>
+        @endauth
+
+        <?= $article->article_content ?>
     </div>
 @endsection
