5a24cb91b07baccafd405afc2d77393a94dc74b0
basileLeroy <basile.leroy.jnm@hotmail.com>
2023-01-15 16:00:07 +0100

    WIP - Add responsiveness to Contact + Thoughts pages

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -10,41 +10,42 @@ belderbos, marc, contact, email, phone, adress, architecturer, about
 @endsection
 
 @section('content')
-    @auth
-        <div class="editSection w3-display-container">
-            <input class="toggle-box" id="header1" type="checkbox" >
-            <label for="header1"><i class="fa fa-edit w3-xxlarge w3-display-topleft"></i></label>
+    <div class="content">
+        @auth
+            <div class="editSection w3-display-container">
+                <input class="toggle-box" id="header1" type="checkbox" >
+                <label for="header1"><i class="fa fa-edit w3-xxlarge w3-display-topleft"></i></label>
 
-            <div class="addSection">
-                <form action="{{ route('contact', ['locale' => app()->getLocale()] ) }}" method="POST" class="sectionUploader">
-                {{ csrf_field() }}
-                    <label for="projectTitle">Adress: </label>
-                    <input type="text" required id="sectionTitle" name="adress" placeholder=" Wegstraat, 26">
-                    <label for="projectTitle">Region / Postal code: </label>
-                    <input type="text" required id="sectionTitle" name="region" placeholder=" 9000, GENT">
-                    <label for="projectTitle">Country: </label>
-                    <input type="text" required id="sectionTitle" name="country" placeholder=" Belgie">
-                    <label for="projectTitle">Phone number: </label>
-                    <input type="text" required id="sectionTitle" name="phone" placeholder=" +32 412 34 56 78">
-                    <label for="projectTitle">Email: </label>
-                    <input type="text" required id="sectionTitle" name="email" placeholder=" example@email.com">
+                <div class="addSection">
+                    <form action="{{ route('contact', ['locale' => app()->getLocale()] ) }}" method="POST" class="sectionUploader">
+                        {{ csrf_field() }}
+                        <label for="projectTitle">Adress: </label>
+                        <input type="text" required id="sectionTitle" name="adress" placeholder=" Wegstraat, 26">
+                        <label for="projectTitle">Region / Postal code: </label>
+                        <input type="text" required id="sectionTitle" name="region" placeholder=" 9000, GENT">
+                        <label for="projectTitle">Country: </label>
+                        <input type="text" required id="sectionTitle" name="country" placeholder=" Belgie">
+                        <label for="projectTitle">Phone number: </label>
+                        <input type="text" required id="sectionTitle" name="phone" placeholder=" +32 412 34 56 78">
+                        <label for="projectTitle">Email: </label>
+                        <input type="text" required id="sectionTitle" name="email" placeholder=" example@email.com">
 
-                    @if ($errors->any())
-                        <div class="alert alert-danger">
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
+                        @if ($errors->any())
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
 
-                    <button type="submit" id="uploadNewSection" name="updateContact" value="Upload">Update new contact info</button>
-                </form>
+                        <button type="submit" id="uploadNewSection" name="updateContact" value="Upload">Update new contact info</button>
+                    </form>
+                </div>
             </div>
-        </div>
-    @endauth
-    <div class="fluid intro" style="width: 650px;">
+        @endauth
+
         <h1>Marc BELDERBOS</h1>
         <br>
         <p>
@@ -59,4 +60,5 @@ belderbos, marc, contact, email, phone, adress, architecturer, about
         <br>
         <a class="link" href="mailto:{{ $contact->email }}">{!! $contact->email !!}</a>
     </div>
+
 @endsection
--- resources/views/thoughts.blade.php
+++ resources/views/thoughts.blade.php
@@ -10,31 +10,31 @@
 @endsection
 
 @section('content')
-    @auth
-        <div class="editSection w3-display-container">
-            <input class="toggle-box" id="header1" type="checkbox" >
-            <label for="header1"><i class="fa fa-edit w3-xxlarge w3-display-topleft"></i></label>
-            <div class="addSection">
-                <form action="{{ route('gedachten', ['locale' => app()->getLocale()] ) }}" method="POST">
-                {{ csrf_field() }}
-                    <textarea class="description" id="sectionContent" name="description">
+    <div class="content">
+        @auth
+            <div class="editSection w3-display-container">
+                <input class="toggle-box" id="header1" type="checkbox" >
+                <label for="header1"><i class="fa fa-edit w3-xxlarge w3-display-topleft"></i></label>
+                <div class="addSection">
+                    <form action="{{ route('gedachten', ['locale' => app()->getLocale()] ) }}" method="POST">
+                        {{ csrf_field() }}
+                        <textarea class="description" id="sectionContent" name="description">
 
                         @foreach ($articles as $article)
-                            {{ $article->article_content }}
-                        @endforeach
+                                {{ $article->article_content }}
+                            @endforeach
 
                     </textarea>
-                    <button type="submit" id="uploadNewSection" name="uploadNewProject" value="Upload">Save</button>
-                </form>
+                        <button type="submit" id="uploadNewSection" name="uploadNewProject" value="Upload">Save</button>
+                    </form>
+                </div>
             </div>
-        </div>
-    @endauth
-    <div class="fluid intro" style="width: 650px;">
+        @endauth
         @foreach ($articles as $article)
             <?= $article->article_content ?>
         @endforeach
-    </div>
-    <div class="fluid book" style="margin-top:50px">
-        <a class="thoughts" href="mailto:{{ env('MAIL_ADMIN') }}"><button>{!! __('pagination.thoughts') !!}</button></a>
+        <a class="thoughts" href="mailto:{{ env('MAIL_ADMIN') }}">
+            <button>{!! __('pagination.thoughts') !!}</button>
+        </a>
     </div>
 @endsection
