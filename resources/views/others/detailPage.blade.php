bd2449fe72c612d78a63659ff660233548169e02
basileLeroy <basile.leroy.jnm@hotmail.com>
2023-01-15 15:48:46 +0100

    WIP - Add responsiveness to Others page + detail pages

--- resources/views/others/detailPage.blade.php
+++ resources/views/others/detailPage.blade.php
@@ -10,39 +10,43 @@
 @endsection
 
 @section('content')
-    <h3>
+    <div class="content">
         @auth
-            <a href="{{ route('deleteOthersArticle', ['article' => $articles->title, 'locale' => app()->getLocale() ] )  }}" style="position:absolute; left:100px"><button style="background-color: lightcoral; color: white; border-radius: 5px;padding:10px;">Delete</button></a>
-        @endauth
-    </h3>
-    @auth
-        <div class="editSection w3-display-container">
-            <input class="toggle-box" id="header1" type="checkbox" >
-            <label for="header1"><i class="fa fa-edit w3-xxlarge w3-display-topleft"></i></label>
-            <div class="addSection" style="width: 170%;">
-                <form action="{{ route('updateDetailPage', ['words' => $articles->title, 'locale' => app()->getLocale()] ) }}" method="POST" class="sectionUploader" enctype="multipart/form-data" >
-                    {{ csrf_field() }}
-                    <label for="image">Add Cover Image</label>
-                    <input type="file" id="image" name="image">
-                    <br><br>
-                    <label for="languageTitle">Add <strong>{{app()->getLocale()}}</strong> Title</label>
-                    <input type="text" id="languageTitle" name="languageTitle" style="border: 1px solid black; padding-left: 5px;">
-                    <br><br>
-                    <textarea class="description" id="sectionContent" name="description">
+            <div class="editSection w3-display-container">
+                <input class="toggle-box" id="header1" type="checkbox" >
+                <label for="header1"><i class="fa fa-edit w3-xxlarge w3-display-topleft"></i></label>
+                <div class="addSection" style="width: 170%;">
+                    <form action="{{ route('updateDetailPage', ['words' => $articles->title, 'locale' => app()->getLocale()] ) }}" method="POST" class="sectionUploader" enctype="multipart/form-data" >
+                        {{ csrf_field() }}
+                        <label for="image">Add Cover Image</label>
+                        <input type="file" id="image" name="image">
+                        <br><br>
+                        <label for="languageTitle">Add <strong>{{app()->getLocale()}}</strong> Title</label>
+                        <input type="text" id="languageTitle" name="languageTitle" style="border: 1px solid black; padding-left: 5px;">
+                        <br><br>
+                        <textarea class="description" id="sectionContent" name="description">
 
                             {!! $articles->article_content !!}
 
                     </textarea>
-                    <button type="submit" id="updateArticle" name="updateArticle" value="Upload" style="padding: 10px; margin-top: 5px">Save</button>
-                </form>
+                        <button type="submit" id="updateArticle" name="updateArticle" value="Upload" style="padding: 10px; margin-top: 5px">Save</button>
+                    </form>
+                </div>
             </div>
+        @endauth
+        <div x-data class="project-detail-card">
+            <?php $title = str_replace("-", " ", $articles->title) ?>
+            <h3>
+                {!! ucwords($title) !!}
+            </h3>
+            @auth
+                <a @click="alert('Are you sure you want to delete this?')" href="{{ route('deleteOthersArticle', ['article' => $articles->title, 'locale' => app()->getLocale() ] )  }}" ><button class="delete-item">Delete</button></a>
+            @endauth
         </div>
-    @endauth
-    <div class="fluid intro" style="width: 650px;">
-            {!! $articles->article_content !!}
-            <br>
-            @if($articles->article_image)
-                <img src="{{ asset('storage/' . $articles->article_image) }}" alt="Others">
-            @endif
+        {!! $articles->article_content !!}
+        <br>
+        @if($articles->article_image)
+            <img src="{{ asset('storage/' . $articles->article_image) }}" alt="Others">
+        @endif
     </div>
 @endsection
--- resources/views/others/others.blade.php
+++ resources/views/others/others.blade.php
@@ -10,56 +10,60 @@
 @endsection
 
 @section('content')
-    @auth
-        <div class="editSection w3-display-container">
-            <input class="toggle-box" id="header1" type="checkbox" >
-            <label for="header1"><i class="fa fa-edit w3-xxlarge w3-display-topleft"></i></label>
-            <div class="addSection" style="width: 170%;">
-                <form action="{{ route('updateOthers', ['locale' => app()->getLocale()] ) }}" method="POST" class="sectionUploader" enctype="multipart/form-data" >
-                    {{ csrf_field() }}
-                    <hr style="margin-top: 20px; border-top: 4px solid #000;">
-                    <h3 style="margin-bottom: 50px;">Update Introduction Text:</h3>
+    <div class="content">
+        @auth
+            <div class="editSection w3-display-container">
+                <input class="toggle-box" id="header1" type="checkbox" >
+                <label for="header1"><i class="fa fa-edit w3-xxlarge w3-display-topleft"></i></label>
+                <div class="addSection" style="width: 170%;">
+                    <form action="{{ route('updateOthers', ['locale' => app()->getLocale()] ) }}" method="POST" class="sectionUploader" enctype="multipart/form-data" >
+                        {{ csrf_field() }}
+                        <hr style="margin-top: 20px; border-top: 4px solid #000;">
+                        <h3 style="margin-bottom: 50px;">Update Introduction Text:</h3>
 
-                    <textarea class="description" id="sectionContent" name="description">
+                        <textarea class="description" id="sectionContent" name="description">
 
                         @foreach ($articles as $article)
-                            {{ $article->article_content }}
-                        @endforeach
+                                {{ $article->article_content }}
+                            @endforeach
 
                     </textarea>
 
-                    <hr style="margin-top: 20px; border-top: 4px solid #000;">
-                    <h3 style="margin-bottom: 50px;">Add New Article:</h3>
+                        <hr style="margin-top: 20px; border-top: 4px solid #000;">
+                        <h3 style="margin-bottom: 50px;">Add New Article:</h3>
 
-                    <label for="newArticle">Title of the new article: </label>
-                    <input type="text" id="newArticle" name="newArticle" style="border: 1px solid black; padding-left: 5px;">
-                    <br>
-                    <label for="image">Add Cover Image</label>
-                    <input type="file" id="image" name="image">
-                    <br>
-                    <button type="submit" id="updateArticle" name="updateArticle" value="Upload" style="margin-top: 10px; padding: 10px;">Save</button>
-                </form>
+                        <label for="newArticle">Title of the new article: </label>
+                        <input type="text" id="newArticle" name="newArticle" style="border: 1px solid black; padding-left: 5px;">
+                        <br>
+                        <label for="image">Add Cover Image</label>
+                        <input type="file" id="image" name="image">
+                        <br>
+                        <button type="submit" id="updateArticle" name="updateArticle" value="Upload" style="margin-top: 10px; padding: 10px;">Save</button>
+                    </form>
+                </div>
             </div>
+        @endauth
+        <div x-data class="project-detail-card">
+            @foreach ($articles as $article)
+                {!! $article->article_content !!}
+                <br>
+            @endforeach
+            <hr>
+            @foreach ($detailPages as $article)
+                <div class="project-card">
+                    <a href="{{ route('otherPages', ['article' => $article->title, 'locale' => app()->getLocale() ] )  }}" class="fullsizable">
+                        <img alt="{{ $article->language_title }}" title="{{ $article->language_title }}" src="{{ asset('storage/' . $article->image)}}">
+                        <p>{{ ucwords($article->language_title) }}</p>
+                    </a>
+                </div>
+            @endforeach
         </div>
-    @endauth
+    </div>
+
 <div class="fluid intro" style="width: 650px;">
-    @foreach ($articles as $article)
-        {!! $article->article_content !!}
-        <br>
-    @endforeach
-        <hr>
+
 </div>
 
     <div class="fluid projects">
-
-        @foreach ($detailPages as $article)
-
-            <div class="project-card">
-                <a href="{{ route('otherPages', ['article' => $article->title, 'locale' => app()->getLocale() ] )  }}" class="fullsizable">
-                    <img alt="{{ $article->language_title }}" title="{{ $article->language_title }}" src="{{ asset('storage/' . $article->image)}}">
-                    <p>{{ ucwords($article->language_title) }}</p>
-                </a>
-            </div>
-        @endforeach
     </div>
 @endsection
