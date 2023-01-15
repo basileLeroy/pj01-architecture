ab4431502b24e6b889434339b04c7ad8e763bf70
basileLeroy <basile.leroy.jnm@hotmail.com>
2023-01-15 18:28:15 +0100

    WIP - Add desktop responsiveness to landing and header

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -6,22 +6,22 @@
     </div>
     <div class="nav-desktop">
         <nav>
-            <ul class="fluid nav">
+            <ul class="primary-navigation">
                 <li><a href="{{ route('intenties-van-de-site', ['locale' => app()->getLocale()] ) }}">{{ __('nav.intenties')}}</a>
-                    <ul class="subnav">
+                    <ul class="secondary-navigation">
                         <li><a href="{{ route('intenties-van-de-site', ['locale' => app()->getLocale()] ) }}">{{ __('nav.intentiesVanDeSite')}}</a></li>
                         <li><a href="{{ route('intenties-bij-een-ontwerp', ['locale' => app()->getLocale()] ) }}">{{ __('nav.intentiesBijEenOntwerp')}}</a></li>
                     </ul>
                 </li>
                 <li><a href="{{ route('architectuur', ['locale' => app()->getLocale()] ) }}">{{ __('nav.architectuur')}}</a></li>
                 <li><a href="{{ route('woorden', ['locale' => app()->getLocale()] ) }}">{{ __('nav.woorden')}}</a>
-                    <ul class="subnav">
+                    <ul class="secondary-navigation">
                         <li><a href="{{ route('woorden', ['locale' => app()->getLocale()] ) }}">Marc Belderbos</a></li>
                         <li><a href="{{ route('anderen', ['locale' => app()->getLocale()] ) }}">{{ __('nav.anderen')}}</a></li>
                     </ul>
                 </li>
                 <li><a href="{{ route('biografie', ['locale' => app()->getLocale()] ) }}">{{ __('nav.ontwerper')}}</a>
-                    <ul class="subnav">
+                    <ul class="secondary-navigation">
                         <li><a href="{{ route('contact', ['locale' => app()->getLocale()] ) }}">contact</a></li>
                         <li><a href="{{ route('biografie', ['locale' => app()->getLocale()] ) }}">{{ __('nav.bio')}}</a></li>
                     </ul>
@@ -32,11 +32,11 @@
         </nav>
         <div class="lang">
             <ul>
-            @foreach (['nl', 'fr', 'en'] as $lang)
-                <li>
-                    <a href="{{ route(Route::CurrentRouteName(), array_merge(request()->route()->parameters, ['locale' => $lang])) }}">{{ $lang }}</a>
-                </li>
-            @endforeach
+                @foreach (['nl', 'fr', 'en'] as $lang)
+                    <li>
+                        <a href="{{ route(Route::CurrentRouteName(), array_merge(request()->route()->parameters, ['locale' => $lang])) }}">{{ $lang }}</a>
+                    </li>
+                @endforeach
             </ul>
         </div>
     </div>
@@ -54,13 +54,13 @@
                 </li>
                 <li><a href="{{ route('architectuur', ['locale' => app()->getLocale()] ) }}">{{ __('nav.architectuur')}}</a></li>
                 <li><a href="{{ route('woorden', ['locale' => app()->getLocale()] ) }}">{{ __('nav.woorden')}}</a>
-                    <ul class="subnav">
+                    <ul class="secondary-navigation">
                         <li><a href="{{ route('woorden', ['locale' => app()->getLocale()] ) }}">Marc Belderbos</a></li>
                         <li><a href="{{ route('anderen', ['locale' => app()->getLocale()] ) }}">{{ __('nav.anderen')}}</a></li>
                     </ul>
                 </li>
                 <li><a href="{{ route('biografie', ['locale' => app()->getLocale()] ) }}">{{ __('nav.ontwerper')}}</a>
-                    <ul class="subnav">
+                    <ul class="secondary-navigation">
                         <li><a href="{{ route('contact', ['locale' => app()->getLocale()] ) }}">contact</a></li>
                         <li><a href="{{ route('biografie', ['locale' => app()->getLocale()] ) }}">{{ __('nav.bio')}}</a></li>
                     </ul>
