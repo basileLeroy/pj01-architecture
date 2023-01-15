9976761c058102c562585c0442e198f6fbd5a2ae
basileLeroy <basile.leroy.jnm@hotmail.com>
2023-01-15 15:05:50 +0100

    WIP - Add responsiveness to project + detail pages

--- resources/views/projects/architecture.blade.php
+++ resources/views/projects/architecture.blade.php
@@ -10,30 +10,29 @@ belderbos, marc, architecturer, projects, projecten, projets, portfolio, achieve
 @endsection
 
 @section('content')
-    @auth
-    <div class="editSection w3-display-container">
-        <input class="toggle-box" id="header1" type="checkbox" >
-        <label for="header1"><i class="fa fa-edit w3-xxlarge w3-display-topleft"></i></label>
+    <div class="content">
+        @auth
+            <div class="editSection w3-display-container">
+                <input class="toggle-box" id="header1" type="checkbox" >
+                <label for="header1"><i class="fa fa-edit w3-xxlarge w3-display-topleft"></i></label>
 
-        <div class="addSection" style="width:170%">
-            <form action="{{ route('architectuur', ['locale' => app()->getLocale()] ) }}" method="POST" class="sectionUploader" enctype="multipart/form-data">
-            {{ csrf_field() }}
-                <h3 style="margin-bottom: 50px;">Add a New Project</h3>
+                <div class="addSection" style="width:170%">
+                    <form action="{{ route('architectuur', ['locale' => app()->getLocale()] ) }}" method="POST" class="sectionUploader" enctype="multipart/form-data">
+                        {{ csrf_field() }}
+                        <h3 style="margin-bottom: 50px;">Add a New Project</h3>
 
-                <label for="projectTitle">Project title: </label>
-                <input type="text" id="sectionTitle" name="projectTitle" placeholder=" (example: 1978-Reel-Boom)">
-                <label for="projectCover">Cover image: </label>
-                <input type="file" id="sectionCover" name="projectCover">
-                <br>
-                <label for="projectGallery">Gallery images: </label>
-                <input type="file" id="sectionGallery" name="projectGallery[]" multiple>
-                <button type="submit" id="uploadNewSection" name="uploadNewProject" value="Upload">Upload new project</button>
-            </form>
-        </div>
-
-    </div>
-    @endauth
-    <div class="fluid projects">
+                        <label for="projectTitle">Project title: </label>
+                        <input type="text" id="sectionTitle" name="projectTitle" placeholder=" (example: 1978-Reel-Boom)">
+                        <label for="projectCover">Cover image: </label>
+                        <input type="file" id="sectionCover" name="projectCover">
+                        <br>
+                        <label for="projectGallery">Gallery images: </label>
+                        <input type="file" id="sectionGallery" name="projectGallery[]" multiple>
+                        <button type="submit" id="uploadNewSection" name="uploadNewProject" value="Upload">Upload new project</button>
+                    </form>
+                </div>
+            </div>
+        @endauth
 
         @foreach ($projects as $project)
         <?php $title = str_replace("-", " ", $project->title) ?>
--- resources/views/projects/project.blade.php
+++ resources/views/projects/project.blade.php
@@ -10,63 +10,62 @@ belderbos, marc, architecturer, <?php echo str_replace("-", ", ", $project) ?>
 @endsection
 
 @section('content')
-<div class="project-gallery">
-    <div class="fluid projects">
-        <div class="project-card">
-            <?php
-                $title = str_replace("-", " ", $cover->title)
-            ?>
-            <h3>
-                {{ucwords($title)}}
-                @auth
-                    <a href="{{ route('deleteProject', ['project'=>$project, 'locale' => app()->getLocale()] ) }}" style="margin: 10px;"><button style="background-color: lightcoral; color: white; border-radius: 5px;padding:10px;">Delete</button></a>
-                @endauth
-            </h3>
-            <img alt="{{ucwords($title)}}" title="{{ ucwords($title) }}" src="{{ asset('storage/' . $cover->project_image) }}">
-            <a class="thoughts" href="{{ route('gedachten', ['locale' => app()->getLocale()] ) }}"><button>{!! __('pagination.thoughts') !!}</button></a>
+<div class="content">
+    @auth
+        <div class="editSection w3-display-container" >
+            <input class="toggle-box" id="header1" type="checkbox" >
+            <label for="header1"><i class="fa fa-edit w3-xxlarge w3-display-topleft"></i></label>
+
+            <div class="addSection" style="width:170%">
+                <form action="{{ route('updateProject', ['project'=>$project, 'locale' => app()->getLocale()] ) }}" method="POST" class="sectionUploader" enctype="multipart/form-data"  style="min-width: 750px;">
+                    {{ csrf_field() }}
+                    <label for="projectTitle">Project title: </label>
+                    <input type="text" id="sectionTitle" name="projectTitle" placeholder=" (example: 1978-Reel-Boom)">
+                    <label for="projectCover">Cover image: </label>
+                    <input type="file" id="sectionCover" name="projectCover">
+                    <br>
+                    <label for="projectGallery">Gallery images: </label>
+                    <input type="file" id="sectionGallery" name="projectGallery[]" multiple>
+                    <br>
+                    <label for="description">About This project</label>
+                    <textarea class="description" id="sectionContent" name="description">
+                        @if($cover->description != null)
+                            {{$cover->description}}
+                        @endif
+                    </textarea>
+                    <button type="submit" id="uploadNewSection" name="uploadNewProject" value="Upload">Upload new project</button>
+                </form>
+            </div>
         </div>
+    @endauth
+
+    <div x-data class="project-detail-card">
+        <?php
+            $title = str_replace("-", " ", $cover->title)
+        ?>
+        <h3>
+{{--            {{ucwords($title)}}--}}
+            {!! ucwords($title) !!}
+
+        </h3>
+        @auth
+            <a @click="alert('Are you sure you want to delete this?')" href="{{ route('deleteProject', ['project'=>$project, 'locale' => app()->getLocale()] ) }}"><button class="delete-item">Delete</button></a>
+        @endauth
+        <img alt="{{ucwords($title)}}" title="{{ ucwords($title) }}" src="{{ asset('storage/' . $cover->project_image) }}">
+        <a class="thoughts" href="{{ route('gedachten', ['locale' => app()->getLocale()] ) }}"><button>{!! __('pagination.thoughts') !!}</button></a>
     </div>
 
-    <div class="fluid intro">
-        <div class="gallery">
+    <div class="gallery">
         @if($imageArray)
             @foreach($imageArray as $sliderImage)
                 <a href="{{ asset('storage/' . $sliderImage) }}" data-lightbox="roadtrip"><img src="{{ asset('storage/' . $sliderImage) }}" alt="{{$title}}"></a>
             @endforeach
         @endif
-        </div>
     </div>
-</div>
-@auth
-    <div class="editSection w3-display-container" >
-        <input class="toggle-box" id="header1" type="checkbox" >
-        <label for="header1"><i class="fa fa-edit w3-xxlarge w3-display-topleft"></i></label>
 
-        <div class="addSection" style="width:170%">
-            <form action="{{ route('updateProject', ['project'=>$project, 'locale' => app()->getLocale()] ) }}" method="POST" class="sectionUploader" enctype="multipart/form-data"  style="min-width: 750px;">
-                {{ csrf_field() }}
-                <label for="projectTitle">Project title: </label>
-                <input type="text" id="sectionTitle" name="projectTitle" placeholder=" (example: 1978-Reel-Boom)">
-                <label for="projectCover">Cover image: </label>
-                <input type="file" id="sectionCover" name="projectCover">
-                <br>
-                <label for="projectGallery">Gallery images: </label>
-                <input type="file" id="sectionGallery" name="projectGallery[]" multiple>
-                <br>
-                <label for="description">About This project</label>
-                <textarea class="description" id="sectionContent" name="description">
-                    @if($cover->description != null)
-                        {{$cover->description}}
-                    @endif
-                </textarea>
-                <button type="submit" id="uploadNewSection" name="uploadNewProject" value="Upload">Upload new project</button>
-            </form>
-        </div>
-    </div>
-@endauth
-<div class="fluid intro" style="width: 650px; margin-top: 50px;">
     @if($cover->description != null)
         {!! $cover->description !!}
     @endif
 </div>
+
 @endsection
