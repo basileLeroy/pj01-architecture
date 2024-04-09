c29fe59648215c4a83fde9ddb7ef3360951af0fd
basileLeroy <basile.leroy.jnm@hotmail.com>
2024-04-09 09:39:03 +0200

    Add default DB structure

---
+++ database/migrations/2024_04_09_073346_create_articles_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('article_content');
+            $table->string('language');
+            $table->timestamps();
+            $table->timestamp('published_at')->nullable();
+            $table->string('page');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
