3674e13492bb3843774296be4074132fdab3ea5f
basileLeroy <basile.leroy.jnm@hotmail.com>
2024-04-12 11:40:06 +0200

    Create show blade view for individual articles

---
+++ resources/views/pages/guest/words/show.blade.php
@@ -0,0 +1,26 @@
+@extends('layout')
+
+@section('seo')
+belderbos, marc, architecturer, words, woorden, mots
+@endsection
+
+@section('title')
+    Mots
+@endsection
+
+@section('content')
+    <div class="content">
+        <div x-data class="project-detail-card">
+            <h3>
+                {!! ucwords($article->title) !!}
+            </h3>
+            <br>
+            @if($article->cover)
+                <img src="{{ asset($article->cover) }}" alt="cover picture of {{$article->title}}">
+            @endif
+        </div>
+        <div class="text-box">
+            {!! $article->content !!}
+        </div>
+    </div>
+@endsection
