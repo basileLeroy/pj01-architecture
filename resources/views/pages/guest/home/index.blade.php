ee5eb1668e1edc33260fd5ba25601799b71d05e5
basileLeroy <basile.leroy.jnm@hotmail.com>
2024-04-15 11:39:07 +0200

    Update no content text on home page

--- resources/views/pages/guest/home/index.blade.php
+++ resources/views/pages/guest/home/index.blade.php
@@ -12,7 +12,7 @@
 <div x-data="{ show: false }"  class="content">
     <img src="{{asset('allessandro.jpg')}}" title="Les ordres - Alessandro Anselmi" alt="Allessandro Anselmi">
 
-    <h1 class="text-show-action" @click="show = !show">{{$article->title}}</h1>
+    <h1 class="text-show-action" @click="show = !show">{!! $article->title ?? "" !!}</h1>
 
     <div id="article-content" class="text-box" x-show="show">
         {!! $article->content ?? __('error.no_content') !!}
