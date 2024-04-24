be73494d49030a2e469c9d51f89bb5cde851774f
basileLeroy <basile.leroy.jnm@hotmail.com>
2024-04-24 20:28:20 +0200

    Remove unused line

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -1,13 +1,10 @@
 // import "../css/admin.css"
 import Sortable from 'sortablejs';
 
-// Sortable.mount(new AutoScroll());
-
-
 // const projects = document.querySelectorAll("#project-item");
 const projectsContainer = document.querySelector("#projects-list");
 
 Sortable.create(projectsContainer, {
     animation: 150,
-    
+    scroll: true,
 });
