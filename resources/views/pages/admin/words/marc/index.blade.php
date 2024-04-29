40efb108ea67a964aea97c6b55345a950d7dd58b
basileLeroy <basile.leroy.jnm@hotmail.com>
2024-04-29 10:45:05 +0200

    Reformat Words dashboard pages

--- resources/views/pages/admin/words/marc/index.blade.php
+++ resources/views/pages/admin/words/marc/index.blade.php
@@ -9,7 +9,7 @@
 @endsection
 
 @section('content')
-@vite(["resources/js/admin.js"])
+    @vite(['resources/js/admin.js'])
 
     <div class="w-full mx-12 flex flex-col items-center ">
         <h1 class="text-3xl font-bold my-12">Mots - Marc Belderbos</h1>
@@ -19,7 +19,7 @@
             <input type="hidden" name="is_primary" value="1">
             <input type="hidden" name="author" value="Marc Belderbos">
             <input type="hidden" name="slug" value="words-by-marc">
-            
+
             <div id="accordion-collapse" class="w-full" data-accordion="collapse">
                 <h2 class="text-2xl mb-6 font-bold">Mettre a jour l'article d'introduction</h2>
 
@@ -95,8 +95,8 @@
                             <span>{{ strtoupper($article->language) }}</span>
                             <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true"
                                 xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M9 5 5 1 1 5" />
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="2" d="M9 5 5 1 1 5" />
                             </svg>
                         </button>
                     </h2>
@@ -134,7 +134,7 @@
             @csrf
             <input type="hidden" name="is_primary" value="0">
             <input type="hidden" name="author" value="Marc Belderbos">
-            
+
             <h2 class="text-2xl font-bold">Creer un article</h2>
 
             <div class="my-5">
@@ -174,41 +174,53 @@
         <div class="w-full flex flex-wrap gap-7 justify-around bg-slate-100 p-12 mb-12 rounded-md">
             <h2 class="text-2xl font-bold w-full self-center">Les mots de Marc</h2>
 
-            <form id="words-list" action="{{route("admin.words.marc.update-order")}}" method="post" id="articles-list" class="w-full flex flex-wrap gap-7 justify-around bg-slate-100 p-12 mb-12 rounded-md">
-                @csrf   
+            <form id="words-list" action="{{ route('admin.words.marc.update-order') }}" method="post"
+                id="articles-list" class="w-full flex flex-wrap gap-7 justify-around bg-slate-100 p-12 mb-12 rounded-md">
+                @csrf
                 @foreach ($articles as $article)
-                <div id="article-item-{{$article->id}}" draggable="true" class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <input type="hidden" name="articles[]" value="{{$article->slug}}">
-                    <div class="flex justify-end px-4 pt-4">
-                        <button id="{{$article->slug}}" data-dropdown-toggle="dropdown-{{$article->id}}" class="inline-block text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-1.5" type="button">
-                            <span class="sr-only">Open dropdown</span>
-                            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 3">
-                                <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z"/>
-                            </svg>
-                        </button>
-                        <!-- Dropdown menu -->
-                        <div id="dropdown-{{$article->id}}" class="z-10 hidden text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-                            <ul class="py-2" aria-labelledby="{{$article->slug}}">
-                            <li>
-                                <a href="{{route("admin.words.marc.editDetail", ["Word" => $article->slug])}}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Edit</a>
-                            </li>
-                            <li>
-                                <a href="#" class="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Delete</a>
-                            </li>
-                            </ul>
+                    <div id="article-item-{{ $article->id }}" draggable="true"
+                        class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <input type="hidden" name="articles[]" value="{{ $article->slug }}">
+                        <div class="flex justify-end px-4 pt-4">
+                            <button id="{{ $article->slug }}" data-dropdown-toggle="dropdown-{{ $article->id }}"
+                                class="inline-block text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-1.5"
+                                type="button">
+                                <span class="sr-only">Open dropdown</span>
+                                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                    fill="currentColor" viewBox="0 0 16 3">
+                                    <path
+                                        d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z" />
+                                </svg>
+                            </button>
+                            <!-- Dropdown menu -->
+                            <div id="dropdown-{{ $article->id }}"
+                                class="z-10 hidden text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                                <ul class="py-2" aria-labelledby="{{ $article->slug }}">
+                                    <li>
+                                        <a href="{{ route('admin.words.marc.editDetail', ['Word' => $article->slug]) }}"
+                                            class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Edit</a>
+                                    </li>
+                                    <li>
+                                        <a href="#"
+                                            class="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Delete</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="cursor-move flex flex-col items-center pb-10">
+                            <img class="h-36 w-auto mb-3 rounded-md shadow-lg" src="{{ asset($article->cover) }}"
+                                alt="Bonnie image" />
+                            <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">{{ $article->title }}</h5>
+                            <span class="text-sm text-gray-500 dark:text-gray-400">{{ $article->updated_at }}</span>
+
                         </div>
                     </div>
-                    <div class="cursor-move flex flex-col items-center pb-10">
-                        <img class="h-36 w-auto mb-3 rounded-md shadow-lg" src="{{asset($article->cover)}}" alt="Bonnie image"/>
-                        <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">{{$article->title}}</h5>
-                        <span class="text-sm text-gray-500 dark:text-gray-400">{{$article ->updated_at}}</span>
-        
-                    </div>
-                </div>
                 @endforeach
                 <div class="flex mt-20 w-full justify-center gap-16">
-                    <button type="submit" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-md px-14 py-2.5 me-2 mb-2  dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Save</button>
-                    <a href="{{ route("admin.projects.create")}}" class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900">Cancel</a>
+                    <button type="submit"
+                        class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-md px-14 py-2.5 me-2 mb-2  dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Save</button>
+                    <a href="{{ route('admin.projects.create') }}"
+                        class="focus:outline-none text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:focus:ring-yellow-900">Cancel</a>
                 </div>
             </form>
         </div>
--- resources/views/pages/admin/words/others/edit.blade.php
+++ resources/views/pages/admin/words/others/edit.blade.php
@@ -16,9 +16,9 @@
             @csrf
             <input type="hidden" name="is_primary" value="0">
             <input type="hidden" name="author" value="Marc Belderbos">
-            
+
             <div id="accordion-collapse" class="w-full" data-accordion="collapse">
-                <h2 class="text-2xl mb-6 font-bold">Mettre a jour "{{$page->title}}"</h2>
+                <h2 class="text-2xl mb-6 font-bold">Mettre a jour "{{ $page->title }}"</h2>
                 <h2 id="accordion-collapse-heading-cover">
                     <button type="button"
                         class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
@@ -32,15 +32,18 @@
                         </svg>
                     </button>
                 </h2>
-                <div id="accordion-collapse-body-cover" class="hidden"
-                    aria-labelledby="accordion-collapse-heading-cover">
+                <div id="accordion-collapse-body-cover" class="hidden" aria-labelledby="accordion-collapse-heading-cover">
 
                     <div class="flex items-center gap-6 p-5 border border-b-0 border-gray-200 dark:border-gray-700 w-full">
-                        <img id="cover-display" class="max-h-28 w-auto mx-5 rounded-lg" src="{{asset($page->cover)}}" alt="Cover image for {{$page->title}}">
+                        <img id="cover-display" class="max-h-28 w-auto mx-5 rounded-lg" src="{{ asset($page->cover) }}"
+                            alt="Cover image for {{ $page->title }}">
                         <div class="mb-5 w-full">
-                            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="cover-image">Changer l'image de couverture</label>
-                            <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="cover-image" name="cover" type="file">
-                        </div>                    
+                            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                for="cover-image">Changer l'image de couverture</label>
+                            <input
+                                class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                                id="cover-image" name="cover" type="file">
+                        </div>
                     </div>
                 </div>
                 @foreach ($articles as $article)
@@ -59,12 +62,15 @@
                     </h2>
                     <div id="accordion-collapse-body-{{ $article->language }}" class="hidden"
                         aria-labelledby="accordion-collapse-heading-{{ $article->language }}">
-                        <input type="hidden" name="{{ $article->language }}[slug]" value="{{$article->slug}}">
+                        <input type="hidden" name="{{ $article->language }}[slug]" value="{{ $article->slug }}">
 
                         <div class="flex flex-col gap-6 p-5 border border-b-0 border-gray-200 dark:border-gray-700 w-full">
                             <div class="w-1/2">
-                                <label for="title-{{$article->language}}" class="block mb-2 text-md font-medium text-gray-900 dark:text-white">Title</label>
-                                <input type="text" value="{{$article->title}}" name="{{$article->language}}[title]" id="title-{{$article->language}}" class="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">    
+                                <label for="title-{{ $article->language }}"
+                                    class="block mb-2 text-md font-medium text-gray-900 dark:text-white">Title</label>
+                                <input type="text" value="{{ $article->title }}" name="{{ $article->language }}[title]"
+                                    id="title-{{ $article->language }}"
+                                    class="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                             </div>
                             <textarea class="w-full" name="{{ $article->language }}[content]" id="tinyMCETextArea" rows="10">{!! $article->content !!}</textarea>
                         </div>
@@ -92,16 +98,16 @@
     <script>
         // interactive update of image on upload
         const fileInput = document.getElementById("cover-image");
-    
+
         fileInput.addEventListener('change', e => {
             const image = document.getElementById("cover-display");
             const file = e.target.files[0];
             const reader = new FileReader();
-    
+
             reader.onload = function(event) {
                 image.src = event.target.result;
             };
-    
+
             reader.readAsDataURL(file);
         });
     </script>
