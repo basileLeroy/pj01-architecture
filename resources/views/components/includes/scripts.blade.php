82c96334bd54aac60b75f3e21714b06d3f28e000
basileLeroy <basile.leroy.jnm@hotmail.com>
2024-05-01 12:21:52 +0200

    Add link and image plugin in TinyMce text editors

--- resources/views/components/includes/scripts.blade.php
+++ resources/views/components/includes/scripts.blade.php
@@ -8,7 +8,12 @@
 <script>
     tinymce.init({
         selector: 'textarea#tinyMCETextArea', // Replace this CSS selector to match the placeholder element for TinyMCE
-        plugins: 'code table lists',
-        toolbar: 'undo redo | blocks | bold italic | alignleft aligncenter alignright | indent outdent | bullist numlist | code | table'
+        plugins: 'code table lists link image',
+        toolbar: 'undo redo | link | image | blocks | bold italic | alignleft aligncenter alignright | indent outdent | bullist numlist | code | table',
+        // formats: {
+        //     // bold : {inline : 'b' },  
+        //     italic : {inline : 'i' },
+        //     //underline : {inline : 'u'}
+        // },
     });
 </script>
