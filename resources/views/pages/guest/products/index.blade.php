bbfd64c4702746685444f9a90f69790c750b7ac6
basileLeroy <basile.leroy.jnm@hotmail.com>
2024-05-01 20:06:50 +0200

    Change Products page

--- resources/views/pages/guest/products/index.blade.php
+++ resources/views/pages/guest/products/index.blade.php
@@ -1,33 +1,17 @@
 @extends('layout')
 
 @section('seo')
-belderbos, marc, architecturer, shop, products, books, book, uitgeverij, edition
+belderbos, biography, bio, architecturer, about
 @endsection
 
 @section('title')
-    {{ __('nav.shop') }}
+    {{ __('nav.shop')}}
 @endsection
 
 @section('content')
     <div class="content">
         <div class="text-box">
-            {{ $primary->content ?? "" }}
-        </div>
-
-        <hr>
-        <div class="card-group">
-            @if ($products->isEmpty())
-            <p>{!! __('error.no_content') !!}</p>
-            @else
-            @foreach ($detailPages as $product)
-                <div class="project-card" >
-                    <a href="{{ route('products.index-' . app()->getLocale(), ['Product' => $product->slug])  }}" class="fullsizable">
-                        <img alt="{{ $product->title }}" title="{{ $product->title }}" src="{{ asset('storage/' . $product->image)}}">
-                        <p>{{ ucwords($product->title) }}</p>
-                    </a>
-                </div>
-            @endforeach
-            @endif
+            {!! $product->content ?? __('error.no_content') !!}
         </div>
     </div>
 @endsection
