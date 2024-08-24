9eb47fb9d3bab01f527c9122c7f97528dd81fc67
basileLeroy <basile.leroy.jnm@hotmail.com>
2024-08-24 15:52:06 +0200

    Remove old code from project detail page

--- resources/views/pages/guest/projects/show.blade.php
+++ resources/views/pages/guest/projects/show.blade.php
@@ -39,37 +39,6 @@ belderbos, marc, architecturer, <?php echo str_replace("-", ", ", $project->titl
         </div>
     </div>
 </div>
-{{-- <div class="content">
-    <div class="card-body">
-        <div x-data class="project-detail-card">
-            <?php
-                $title = str_replace("-", " ", $project->title)
-            ?>
-            <h1>
-                {!! ucwords($title) !!}
-            </h1>
-            <img alt="{{ucwords($title)}}" title="{{ ucwords($title) }}" src="{{ asset($project->cover) }}">
-            <a class="thoughts" href="{{ route('thoughts-' . app()->getLocale()) }}"><button>{!! __('pagination.thoughts') !!}</button></a>
-        </div>
-        <div class="card-content">
-            <div class="gallery" id="" >
-                @if($project->gallery)
-                    @foreach($project->gallery as $image)
-                        <a
-                            href="{{ asset($image) }}"
-                            data-fslightbox="gallery-item"
-                        >
-                            <img src="{{ asset($image) }}" alt="{{$title}}">
-                        </a>
-                    @endforeach 
-                @endif
-            </div>
-            <div class="text-box">
-                {!! $project->description ?? __('error.no_content') !!}
-            </div>
-        </div>
-    </div>
-</div> --}}
 @vite(['resources/js/lightbox/fslightbox.js'])
 
 @endsection
