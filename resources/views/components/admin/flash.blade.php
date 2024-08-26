ade97a54ddaf0f9c034a619ce186f76c34a5f8c7
basileLeroy <basile.leroy.jnm@hotmail.com>
2024-08-26 18:36:59 +0200

    Add timer to flash messages with alpineJS

--- resources/views/components/admin/flash.blade.php
+++ resources/views/components/admin/flash.blade.php
@@ -1,5 +1,6 @@
 @if(session('alert'))
-<div class="fixed bottom-0 z-20 flex items-center p-4 mb-4 border-2 border-red-800 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-500" role="alert">
+<div x-data="{ show: true }" x-init="setTimeout(() => show = false, 10000)" x-show="show" 
+    class="fixed bottom-0 z-20 flex items-center p-4 mb-4 border-2 border-red-800 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-500" role="alert">
     <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
         <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
     </svg>
@@ -11,7 +12,8 @@
 @endif
 
 @if (session('status'))
-<div class="fixed bottom-0 z-20 flex items-center p-4 mb-4 border-2 border-green-800 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
+<div x-data="{ show: true }" x-init="setTimeout(() => show = false, 10000)" x-show="show" 
+    class="fixed bottom-0 z-20 flex items-center p-4 mb-4 border-2 border-green-800 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
     <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
         <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
     </svg>
